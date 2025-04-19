779d3a0e6cc14db7ed9c5e37da754a93d707c632
Alish Shrestha <alish.shrestha@codingmountain.com>
2025-04-19 22:30:09 +0545

    create the header section

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+import { IoMdHome } from "react-icons/io";
+import { BsInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={IoMdHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsInfoCircleFill} />
+      </div>
+      <div className="text-2xl">
+        <Link href="/">
+          <h2>
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  );
+}
