868579972d6a68c568edde47c5f60c95973b2731
Alish Shrestha <alish.shrestha@codingmountain.com>
2025-04-20 16:34:47 +0545

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+            Welcome to our movie database website! We are a team of passionate movie enthusiasts dedicated to providing you with the best movie information and recommendations. Our goal is to help you discover new films, explore different genres, and find your next favorite movie.
+        </p>
+    </div>
+  )
+}
